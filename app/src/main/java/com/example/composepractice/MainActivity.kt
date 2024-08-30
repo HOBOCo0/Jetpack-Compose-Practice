1ecfd8f3f4d2d0638445651cecef4a1506696e8d
aman1 <aman15220@gmail.com>
2024-08-30 23:54:59 +0530

    Basic of compose

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -9,38 +9,32 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.composepractice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposePracticeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Text(text = "Hello")
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun SayHello( x: String){
+    Text(text = "Hello $x",
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.ExtraBold,
+        color = Color.Blue,
+        fontSize = 20.sp
     )
 }
-
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "Hello Message", showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    ComposePracticeTheme {
-        Greeting("Android")
-    }
+private fun Previewfunction(){
+    SayHello(x = "Aman Srivastava")
 }
