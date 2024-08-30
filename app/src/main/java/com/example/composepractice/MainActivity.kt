7c414fb743069362d63f9c96dde0f333a6214743
aman1 <aman15220@gmail.com>
2024-08-31 01:01:40 +0530

    compose Text, Image, Button, TextField

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -1,40 +1,75 @@
 package com.example.composepractice
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
-import com.example.composepractice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Hello")
+            TextInput()
         }
     }
 }
+
 @Composable
-fun SayHello( x: String){
-    Text(text = "Hello $x",
+fun SayHello(x: String) {
+    Text(
+        text = "Hello $x",
         fontStyle = FontStyle.Italic,
         fontWeight = FontWeight.ExtraBold,
         color = Color.Blue,
-        fontSize = 20.sp
+        fontSize = 20.sp,
+        textAlign = TextAlign.Center
     )
 }
-@Preview(showBackground = true, name = "Hello Message", showSystemUi = true)
+
+
 @Composable
-private fun Previewfunction(){
-    SayHello(x = "Aman Srivastava")
+private fun Previewfunction() {
+    //Image
+    //   Image(painter = painterResource(id = R.drawable.mydp) , contentDescription = "Dummy Image", contentScale = ContentScale.Fit)
+    //Text
+    //    SayHello(x = "Aman Srivastava")
+    //Button
+//    Button(onClick = { /*TODO*/ }) {
+//        Text(text = "Hello")
+//        Image(painter = painterResource(id = R.drawable.mydp), contentDescription = "Button Image")
+//    }
+
+//    TextField(value = "Hell My friend",
+//        onValueChange = {
+//
+//        },
+//        label = { Text("Enter Message") },
+//        placeholder = {})
+}
+
+@Composable
+fun TextInput() {
+    val state = remember{ mutableStateOf("")}
+    TextField(value = state.value,
+        onValueChange = {
+            state.value = it
+        },
+        label = { Text("Enter Message") },
+        placeholder = {})
 }
